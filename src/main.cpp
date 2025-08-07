5c8550b743846184b264042fb810cd61fd7b3bf3
Shubham Maheshwari <academicShub@gmail.com>
2025-08-07 10:58:11 +0000

    add clang formatting

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,7 @@ enum class TokenType {
     semi,
 };
 
-struct Token
-{
+struct Token {
     TokenType type;
     std::optional<std::string> value;
 };
@@ -21,48 +20,43 @@ std::vector<Token> tokenize(const std::string& text)
     std::vector<Token> tokens;
     std::string buf;
     int text_size = text.size();
-    for (int i = 0; i < text_size; i++)
-    {
+    for (int i = 0; i < text_size; i++) {
         char c = text.at(i);
-        if (std::isalpha(c))
-        {
+        if (std::isalpha(c)) {
             buf.push_back(c);
             i++;
-            while (std::isalpha(text.at(i)))
-            {
+            while (std::isalpha(text.at(i))) {
                 buf.push_back(text.at(i));
                 i++;
             }
             i--;
-            if (buf == "return")
-            {
-                tokens.push_back(Token{TokenType::_return, std::nullopt});
+            if (buf == "return") {
+                tokens.push_back(Token{ TokenType::_return, std::nullopt });
                 buf.clear();
-            } else
-            {
+            }
+            else {
                 std::cerr << "you messed up" << std::endl;
                 exit(EXIT_FAILURE);
             }
-        } else if (std::isdigit(c))
-        {
+        }
+        else if (std::isdigit(c)) {
             buf.push_back(c);
             i++;
-            while (std::isdigit(text.at(i)))
-            {
+            while (std::isdigit(text.at(i))) {
                 buf.push_back(text.at(i));
                 i++;
             }
             i--;
-            tokens.push_back(Token{TokenType::int_lit, buf});
+            tokens.push_back(Token{ TokenType::int_lit, buf });
             buf.clear();
-        } else if (c == ';')
-        {
-            tokens.push_back(Token{TokenType::semi, std::nullopt});
-        } else if (std::isspace(c))
-        {
+        }
+        else if (c == ';') {
+            tokens.push_back(Token{ TokenType::semi, std::nullopt });
+        }
+        else if (std::isspace(c)) {
             continue;
-        } else
-        {
+        }
+        else {
             std::cerr << "you messed up" << std::endl;
             exit(EXIT_FAILURE);
         }
@@ -75,17 +69,13 @@ std::string tokens_to_asm(const std::vector<Token>& tokens)
     std::stringstream output;
     output << ".global _start\n";
     output << ".section .text\n_start:\n";
-    for (int i = 0; i < tokens.size(); i++)
-    {
+    for (int i = 0; i < tokens.size(); i++) {
         const Token& token = tokens[i];
-        if (token.type == TokenType::_return)
-        {
-            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_lit)
-            {
-                if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semi)
-                {
+        if (token.type == TokenType::_return) {
+            if (i + 1 < tokens.size() && tokens[i + 1].type == TokenType::int_lit) {
+                if (i + 2 < tokens.size() && tokens[i + 2].type == TokenType::semi) {
                     output << "    mov x0, #" << tokens[i + 1].value.value() << "\n";
-                    output << "    mov x8, #93\n" ;
+                    output << "    mov x8, #93\n";
                     output << "    svc #0\n";
                 }
             }
@@ -94,9 +84,9 @@ std::string tokens_to_asm(const std::vector<Token>& tokens)
     return output.str();
 }
 
-int main(const int argc, char *argv[]) {
-    if (argc != 2)
-    {
+int main(const int argc, char* argv[])
+{
+    if (argc != 2) {
         std::cerr << "Incorrect usage. Correct usage is..." << std::endl;
         std::cerr << "hydro <input.hy>" << std::endl;
         return EXIT_FAILURE;
